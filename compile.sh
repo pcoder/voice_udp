c5d0ad086378c327e84aed01553ddffe3eacda27
M.Ravi <mondi.ravi@gmail.com>
2015-08-27 15:57:15 +0200

    Added compilation files

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac src/de/voicechat/*.java -cp lib/weblaf-1.25.jar:lib/jspeex.jar: -d bin/
---
+++ server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ./bin/:lib/jspeex.jar:lib/weblaf-1.25.jar: de.voicechat.Application server
